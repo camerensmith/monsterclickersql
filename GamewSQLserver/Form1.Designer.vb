557417cd1a22ade5a851abe83a8eb3eb41cc5243
break <break@DESKTOP-G9LLK09>
2022-05-02 12:12:36 -0500

    Add project files.

---
+++ GamewSQLserver/Form1.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(323, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(63, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(385, 397)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(22, 25)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "0"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(433, 397)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(19, 25)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "/"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(479, 397)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(22, 25)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "0"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(125, 326)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(112, 112)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(405, 110)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(150, 75)
+        Me.PictureBox1.TabIndex = 5
+        Me.PictureBox1.TabStop = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ GamewSQLserver/Form1.vb
@@ -0,0 +1,116 @@
+﻿
+Imports System.Data.SqlClient
+Imports System
+
+Public Class Form1
+
+
+
+    REM NuGet download packages for SQL server // Package manager for System.Data.SqlClient
+
+
+
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+        Dim myCursor As New Cursor(New IO.MemoryStream(My.Resources.cursor.cursor)) 'Exception here indicates a 32bpp cursor.
+    End Sub
+    ' Change over the cursor to the Pokeball style image
+
+
+    Dim index As Integer
+    Dim Points As Integer
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+
+        loadData(index)
+        Label4.Text = dt.Rows.Count
+        Timer1.Enabled = True
+
+
+
+    End Sub
+
+
+
+
+
+    Dim dt As New DataTable
+    Private Sub loadData(ByVal position As Integer)
+
+
+
+        Dim conn As New SqlConnection("Data Source=.;Initial catalog=games;Integrated Security=true")
+        Dim cmd As New SqlCommand("Select word,path_picture from game1", conn)
+        Dim da As New SqlDataAdapter(cmd)
+
+        da.Fill(dt)
+        'After defining the variables for storage, I fill in the data table that I defined as dt through
+        'the SQL data adapter
+
+        Label1.Text = dt.Rows(position)(0).ToString
+        PictureBox1.Image = Image.FromFile(dt.Rows(position)(1).ToString)
+
+
+
+    End Sub
+
+
+    Private Sub PictureBox1_Click(sender As Object, e As Event Args) Handles PictureBox1.Click
+
+        index = index + 1
+        loadData(index)
+        Points = Points + 1
+        Label2.Text = Points
+
+
+    End Sub
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        PictureBox1.Left = Rnd() * (Me.Width - PictureBox1.Left)
+        PictureBox1.Top = Rnd() * (Me.Width - PictureBox1.Height)
+
+        If PictureBox1.Bounds.IntersectsWith(Button1.Bounds) Then
+            PictureBox1.Top = 230
+            PictureBox1.Left = 180
+        End If
+
+        If PictureBox1.Bounds.IntersectsWith(Label1.Bounds) Then
+            PictureBox1.Top = 230
+            PictureBox1.Left = 180
+        End If
+
+        If PictureBox1.Bounds.IntersectsWith(Label2.Bounds) Then
+            PictureBox1.Top = 230
+            PictureBox1.Left = 180
+        End If
+
+        If PictureBox1.Bounds.IntersectsWith(Label3.Bounds) Then
+            PictureBox1.Top = 230
+            PictureBox1.Left = 180
+        End If
+
+        If PictureBox1.Bounds.IntersectsWith(Label4.Bounds) Then
+            PictureBox1.Top = 230
+            PictureBox1.Left = 180
+        End If
+
+
+        If Label2.Text >= Label4.Text Then
+            Label1.Visible = False
+            PictureBox1.Visible = False
+            Timer1.Enabled = False
+            MessageBox.Show("Thanks for Playing")
+        End If
+
+        ' Buttoning up the game for completion, makes screen go blank and a message box appear
+        'to end the game
+    End Sub
+
+
+
+
+
+End Class
